f01b2501557eac06bdfca654ae34ba796b6d5ffe
vshigurui <v.shigurui@gmail.com>
2022-09-03 22:16:52 +0300

    add Домашние задание к Уроку № 1

---
+++ dzC#/Program.cs
@@ -0,0 +1,72 @@
+﻿    // Задача №2
+
+// int a = 5;
+// int b = 7;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// Console.Write("число ");
+// int a = 2;
+// int b = 10;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// Console.Write("число ");
+// int a = -9;
+// int b = -3;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+    // Задача №4
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// int a = 44;
+// int b = 5;
+// int c = 78;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+    // Задача № 6
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a % 2 == 0) {
+//     Console.WriteLine("Число четное");
+// }
+// else {
+//     Console.WriteLine("Число нечетное");
+// }
+
+
+// Задача № 8
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i <= a; i++) {
+//     if (i % 2 == 0) {
+//         Console.Write(i);
+//         Console.Write(" ");
+//     }
+// }
