d61b6b645493badd19d4e3550ecad4bf2551f26b
vshigurui <v.shigurui@gmail.com>
2022-09-06 10:14:56 +0300

    add домашнее задание урок 1

--- dzC#/Program.cs
+++ dzC#/Program.cs
@@ -1,2 +1,31 @@
-﻿
+﻿    // Задача №10
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write(a % 100);
 
+    // Задача №13
+
+
+
+
+    // Задача № 6
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a % 2 == 0) {
+//     Console.WriteLine("Число четное");
+// }
+// else {
+//     Console.WriteLine("Число нечетное");
+// }
+
+
+// Задача № 8
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i <= a; i++) {
+//     if (i % 2 == 0) {
+//         Console.Write(i);
+//         Console.Write(" ");
+//     }
+// }
