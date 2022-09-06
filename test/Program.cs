d4435dd1e6186af89e09f172f5226d11149195e3
vshigurui <v.shigurui@gmail.com>
2022-09-06 11:04:30 +0300

    add практика онлайн

---
+++ test/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+// Console.WriteLine("Введите координаты двух точек");
+// Console.Write("Введите x1: ");
+//     int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y1: ");
+//     int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите x2: ");
+//     int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y2: ");
+//     int y2 = Convert.ToInt32(Console.ReadLine());
+
+//     double L = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+// Console.WriteLine("Расстояние между точками равно: " + L);
+
+
+
+// Console.Clear();
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+//     Console.WriteLine(i * i);
