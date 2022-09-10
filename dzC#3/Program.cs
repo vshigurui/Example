6f1e67fe37fec3d4687a384b651876fb3315e709
vshigurui <v.shigurui@gmail.com>
2022-09-10 12:47:33 +0300

    add Домашнее задание к Уроку 3

---
+++ dzC#3/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача № 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// Console.Clear();
+
+// Console.Write("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = (n / 10000); 
+// int n2 = (n / 1000) % 10 ;
+// int n4 = (n % 100) / 10;  
+// int n5 = n % 10;
+
+// if (n1 == n5 && n2 == n4 ) {
+//     Console.WriteLine("число палиндром");
+// }
+// else {
+//     Console.WriteLine("число не палиндром");
+// }
+
+
+// Задача № 21  Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+
+// Console.Write("Введите x1 = ");
+//     int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y1 = ");
+//     int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z1 = ");
+//     int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите x2 = ");
+//     int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y2 = ");
+//     int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z2 = ");
+//     int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите x3 = ");
+//     int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y3 = ");
+//     int y3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z3 = ");
+//     int z3 = Convert.ToInt32(Console.ReadLine());
+
+
+//     double L = Math.Sqrt(Math.Pow(x1 - x2 - x3, 2) + Math.Pow(y1 - y2 - y3, 2) + Math.Pow(z1 - z2 - z3, 2));
+//     Console.WriteLine("Расстояния между точками равно L = " + L);
+
+
+
+// Задача № 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++ ) {
+//     Console.WriteLine(i * i * i);
+// }
