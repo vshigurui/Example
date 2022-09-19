87da4b96975466c1ae471f6bed501c7fc3d066ed
vshigurui <v.shigurui@gmail.com>
2022-09-19 21:18:26 +0300

    add домашнее задание урок 6

---
+++ dz6/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+// Console.Write("Введите M чисел(через пробел):  ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int[m];
+// int count = 0;
+
+// void InputArray (int m) {
+//     for (int i = 0; i < m; i++) {
+//         Console.Write($"Введите {i+1} число: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+        
+//     }
+// }
+
+// void countNamber(int [] array ){
+//     for (int j = 0; j < array.Length; j++) {
+//         if (array[j] > 0 ) 
+//             count ++;
+//     }
+//     return ;
+// }
+// InputArray(m);
+// countNamber(array);
+// Console.WriteLine(count);
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Console.WriteLine("введите значение b1");
+//     double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+//     double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+//     double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+//     double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+
+
+
+
+
+
